863017c4cd1add29ea0eaa2ebfdac3a9693d8d41
gtasa3box <gtasa3box@gmail.com>
2022-02-19 14:36:04 +0300

    com

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,22 @@
-console.log('components');
+// services-slider
+$('.owl-services').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    navText: ["<img src='../img/arrow-slider.svg'>","<img src='../img/arrow-slider.svg'>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+})
