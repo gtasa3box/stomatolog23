123b671815a0039917fa9845bdf5458a0443822e
gtasa3box <gtasa3box@gmail.com>
2022-02-19 19:27:51 +0300

    com

--- src/js/_components.js
+++ src/js/_components.js
@@ -43,3 +43,18 @@ $('.owl-specialists').owlCarousel({
         }
     }
 })
+
+// input-tel
+
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    initialCountry: "ru",
+    utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+document.querySelector('#phone').addEventListener('input', 
+    function(e){
+      this.value = this.value.replace(/[^\d.]/g, '');
+    }
+)
